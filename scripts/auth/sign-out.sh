e115cc651d933a650c5e1439e2e0b6461db1c31e
kailinc <kailinc@bu.edu>
2017-08-25 10:47:04 -0400

    Added Curl scrips for changing passwords, sign out
    - works

--- scripts/auth/sign-out.sh
+++ scripts/auth/sign-out.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+TOKEN='+1Ic0KVCBgAyoL0sFSIs52JD97Vhdy22vtW8SXqyq+M=--lKzFdwITjXp1wRlNQGiNiz/ogDlBcDvQjK1yUScxdNQ='
+ID='59a032b03eb6d30d77e9ed5f'
 
 API="http://localhost:4741"
 URL_PATH="/sign-out"
