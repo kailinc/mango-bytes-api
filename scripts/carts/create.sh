4a6f24ca7aeee9a8dac0870e3ea8c1a5aa16c828
kailinc <kailinc@bu.edu>
2017-08-26 13:24:28 -0400

    Tested Auth
    - can't update other people's carts

--- scripts/carts/create.sh
+++ scripts/carts/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TEXT='teststttt'
+TEXT='mmmmmy bucket'
 TOKEN='MJ2dnx5bLZ0+g/hY8REbVsc5J0bawPyK0ZdPW2ji3CU=--Pl5iuMZ9oh/BuW4+bh+Jypd2LGDRAZihxeTfNiArqD0='
 API="http://localhost:4741"
 URL_PATH="/carts"
--- scripts/carts/update.sh
+++ scripts/carts/update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+TEXT='no anymore'
+TOKEN='MJ2dnx5bLZ0+g/hY8REbVsc5J0bawPyK0ZdPW2ji3CU=--Pl5iuMZ9oh/BuW4+bh+Jypd2LGDRAZihxeTfNiArqD0='
+ID='59a1adb45f8229648451d081'
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/carts"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,7 +11,7 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "example": {
+    "cart": {
       "text": "'"${TEXT}"'"
     }
   }'
