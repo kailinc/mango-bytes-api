a77052e7e111a5fede71d7a59ca4671aa3c06598
kailinc <kailinc@bu.edu>
2017-08-26 20:43:46 -0400

    Tested Update cart
    - auth works
    - can only update own cart

--- scripts/carts/update.sh
+++ scripts/carts/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-TEXT='no anymore'
-TOKEN='MJ2dnx5bLZ0+g/hY8REbVsc5J0bawPyK0ZdPW2ji3CU=--Pl5iuMZ9oh/BuW4+bh+Jypd2LGDRAZihxeTfNiArqD0='
-ID='59a1adb45f8229648451d081'
+TOKEN='x+H8wci5MO/UpAbll0dbEl3/gwJW+AmhBQy47HbCBM=--+i3EGuBKKGJNl5dY1kWSnxVQUOTiOq6yuyXJx47Cqsc='
+ID='59a1b16c1236ed66d04203c8'
 API="http://localhost:4741"
 URL_PATH="/carts"
 
@@ -12,7 +11,7 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "cart": {
-      "text": "'"${TEXT}"'"
+      "products": [{"item_id": "59a197c79f399e9a7f9725af", "quantity": "11", "price": "10"}, {"item_id": "59a197c79f399e9a7f9725b0", "quantity": "30", "price": "10"}]
     }
   }'
 
