b28d25bf361474d7203f2102f1adfac901a2e114
kailinc <kailinc@bu.edu>
2017-08-26 20:39:50 -0400

    Tested curl scripts for index, show cart
    - works
    - token used for editable: true/false

--- scripts/carts/destroy.sh
+++ scripts/carts/destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TOKEN='MJ2dnx5bLZ0+g/hY8REbVsc5J0bawPyK0ZdPW2ji3CU=--Pl5iuMZ9oh/BuW4+bh+Jypd2LGDRAZihxeTfNiArqD0='
-ID='59a212138da2eb6a63e45ce7'
+TOKEN='MJ2dnx5bLZ0+g/hY8REbVsc50bawPyK0ZdPW2ji3CU=--Pl5iuMZ9oh/BuW4+bh+Jypd2LGDRAZihxeTfNiArqD0='
+ID='59a1b176e1dedc66e07a7a66'
 API="http://localhost:4741"
 URL_PATH="/carts"
 
--- scripts/carts/show.sh
+++ scripts/carts/show.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
+TOKEN='MJ2dnx5bLZ0+g/hY8REbVsc5J0bawPyK0ZdPW2ji3CU=--Pl5iuMZ9oh/BuW4+bh+Jypd2LGDRAZihxeTfNiArqD0='
+ID='59a1adb45f8229648451d081'
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/carts"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
