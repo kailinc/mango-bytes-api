b3dbeff31e6b57dbf2b009c131844de85e179c90
kailinc <kailinc@bu.edu>
2018-04-16 14:31:31 -0400

    Updated charges controller to process create
    - it will pass in additional data from front end to stripe in creating
      customer, charge
    - stored incoming data into a pojo

--- app/controllers/charges.js
+++ app/controllers/charges.js
@@ -7,36 +7,45 @@ const authenticate = require('./concerns/authenticate')
 const stripe = require('stripe')('sk_test_q60nMuMw7awlmcjHAOQdTSu0')
 
 const create = (req, res, next) => {
-  console.log('create', req)
+  const data = {
+    email: req.body.charge.email,
+    source: req.body.charge.stripeToken,
+    description: req.body.charge.description,
+    shipping: req.body.charge.shipping,
+    amount: req.body.charge.amount,
+    currency: req.body.charge.currency
+  }
   // this data is given to us from the token from stripe
   stripe.customers.create({
     // in your stripe dashboard, this is the displayed email as customer
-    email: req.body.charge.email,
+    email: data.email,
     // their card (it is hashed)
-    source: req.body.charge.stripeToken.token.id
+    source: data.source,
+    description: data.description,
+    shipping: data.shipping
   })
   .then(customer => {
     // the charge itself
     stripe.charges.create({
       // the amount
-      amount: '999',
+      amount: data.amount,
       // the description isn't nescesary but great for us to see later on
-      description: 'Sample Charge',
-      currency: 'usd',
+      description: data.description,
+      currency: data.currency,
       // hashed by stripe and placed in the token for us
       customer: customer.id
     })
-    .then(charge => {
-      res.send(charge)
-      // more of the charge itself
-      Charge.create({
-        // the token id (the credit card)
-        'stripeToken': charge.id,
-        // amount is always in cents
-        'amount': charge.amount,
-        '_owner': req.user._id
-      })
+  .then(charge => {
+    res.send(charge)
+    // more of the charge itself
+    Charge.create({
+      // the token id (the credit card)
+      'stripeToken': charge.id,
+      // amount is always in cents
+      'amount': charge.amount,
+      '_owner': req.user._id
     })
+  })
     // error return
       .catch(error => console.log(error))
   })
